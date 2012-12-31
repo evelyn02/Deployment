e2a6fad2c14b54544a43abf523717f4109c5de57
Evelyn Nomayo <evelyn.nomayo@northeastcollege.ie>
2012-12-31 09:36:39 +0000

    Classroom Need Website
    School teachers in Ireland post classroom project requests on
    ClassroomNeed.ie website. Requests range from Crayons for art class, to
    piano for a school recital, to microscope slides for a biology class.
    
    Then, Donors can browse project requests and give any amount to the one
    that inspires them. Once a project reaches its funding goal, we deliver
    the materials to the school.

---
+++ ClassRoomNeed/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ClassRoomNeed::Application.config.session_store :cookie_store, key: '_ClassRoomNeed_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ClassRoomNeed::Application.config.session_store :active_record_store
